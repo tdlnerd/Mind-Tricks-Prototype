3474f539a04246d762602c088e2e26fe5bf800b0
Tanner <tanner.luce17@beltonnewtech.org>
2015-09-11 16:21:53 -0500

    More materials, improved movement script

--- Assets/Placement Scripts/Move Preassigned.js
+++ Assets/Placement Scripts/Move Preassigned.js
@@ -1,18 +1,27 @@
 ﻿var Pos : Vector3[];
 var Rot : Vector3[];
+var invisible : Material;
+var normalmaterial : Material;
+
+function Start () {
+normalmaterial = GetComponent.<Renderer>().material;
+}
 function OnBecameInvisible () {
 Move();
 }
 
 function Move() {
+	GetComponent.<Renderer>().material = invisible;
 	choice = Random.Range(0,Pos.Length);
 	transform.position = Pos[choice];
 	transform.rotation = Quaternion.Euler(Rot[choice]);
-
+		if (GetComponent.<Renderer>().isvisible == false) {
+		GetComponent.<Renderer>().material = normalmaterial;
+		}
 }
 
-function OnCollisionEnter (other : Collision) { 
-	if (other.gameObject.tag == "Player") {
+function OnCollisionEnter(collision: Collision) {
+	if (collision.gameObject.tag == "Player") {
 		return;
 		}
 	choice = Random.Range(0,Pos.Length);
