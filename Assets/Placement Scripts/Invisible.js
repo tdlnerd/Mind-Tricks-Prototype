9f7bca5cb06b32cdd0d100a1f866217c08fd5c85
Tanner Luce <tdlnerd@gmail.com>
2015-09-13 17:40:14 -0500

    The entire room has been almost completely redone with new furniture models and textures. The lighting has also been improved.

--- Assets/Placement Scripts/Invisible.js
+++ Assets/Placement Scripts/Invisible.js
@@ -3,21 +3,22 @@ var normalmaterial : Material;
 var check : MovePreassigned;
 var doublecheck : Renderer;
 function Start () {
+	check.MatChange = false;
 	if (normalmaterial == null) {
 		normalmaterial = GetComponent.<Renderer>().material;
 	}
 }
 
 function Update () {
-Debug.Log("MAT" + check.MatChange);
 	if (check.MatChange == true) {
 		gameObject.layer = 2;
 	}
 	if (check.MatChange == false) {
 		if (doublecheck.isVisible == false) {
 			gameObject.layer = 0;
-			}
+
 	}
+}
 }	
 		
 	
