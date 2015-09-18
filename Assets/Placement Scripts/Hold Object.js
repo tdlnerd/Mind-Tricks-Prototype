6b1683595b28ad6ddb7a95356ddaca1ddebf1904
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-09-18 10:44:46 -0500

    Objects can now be picked up and held. It works by parenting the object to a holder attached to the main camera.

---
+++ Assets/Placement Scripts/Hold Object.js
@@ -0,0 +1,6 @@
+﻿var Control : GameObject;
+function OnMouseDown () {
+gameObject.transform.parent = Control.transform;
+GetComponent.<Rigidbody>().isKinematic = true; 
+transform.position = Control.transform.position;
+}
