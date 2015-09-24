11fec82097f45cb3eaa6aa7fccedda0134c928bf
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-09-24 10:45:48 -0500

    RUN... COMMIT ALL THE FILES!

---
+++ Assets/Timer.js
@@ -0,0 +1,34 @@
+﻿var timer: float = 70;
+var isFinishedLevel : boolean = false;
+public var displayText : UnityEngine.UI.Text;
+
+private var oldTimer : float;
+
+function Start(){
+	oldTimer = timer;
+}
+
+function Update()
+{
+	if (!isFinishedLevel) {
+		timer -= Time.deltaTime;
+	} 
+
+	if (timer > 0) {
+		var minsDisplay : String = parseInt( timer / 60 ).ToString();
+		var secsDisplay : String = parseInt( timer ).ToString();
+		 
+		if ( (timer - ( parseInt(minsDisplay) * 60)) > 10 ) {
+		     secsDisplay = parseInt( timer - ( parseInt(minsDisplay) * 60) ).ToString();
+		} 
+		else {
+			secsDisplay = "0" + parseInt( timer - 
+                          ( parseInt(minsDisplay) * 60) ).ToString();
+		}
+		
+		displayText.text = minsDisplay + " : " + secsDisplay;
+	} 
+	else {
+	 	timer = oldTimer;
+	}
+}
