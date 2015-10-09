ba68bbbeba113cbade99f7cfb91a752c9b7db86e
Tanner Luce <tdlnerd@gmail.com>
2015-10-08 20:50:34 -0500

    Added ending scene.

---
+++ Assets/FD.js
@@ -0,0 +1,19 @@
+﻿
+var door: Transform; 
+var gotKey : boolean = false; 
+var angleOpen: int; 
+var angleClose: int; 
+var speedOpen: int =1000;
+var RequireKey = false;
+var DoorOpen : AudioClip;
+var DMesh : GameObject;
+
+ 
+function OnTriggerStay (other: Collider) {
+ if(other.gameObject.tag == "FDKey") {
+GetComponent.<AudioSource>().PlayOneShot(DoorOpen);
+Application.LoadLevel("Ending");
+ }
+}
+  
+
