686f6be94d32dd5e76646b4b1440242be01ae1ea
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-10-09 10:44:57 -0500

    Added title screen, will work on game over weekend.

---
+++ Assets/TS.js
@@ -0,0 +1,23 @@
+﻿var Xmin : float;
+var Xmax : float;
+var Ymin : float;
+var Ymax : float;
+var VX = 0;
+var VY = 0;
+var CanSwitch = true;
+
+
+function Update () {
+if (CanSwitch == true) {
+	Dir();
+	}
+	transform.Translate(Vector3(VX,0,VY) * Time.deltaTime);
+}
+
+function Dir () {
+CanSwitch = false;
+VX = Random.Range(Xmin,Xmax);
+VY = Random.Range(Ymin,Ymax);
+yield WaitForSeconds(Random.Range(0,10));
+CanSwitch = true;
+}
